df1e8089102847e4cc009ded47b15900facf1a92
hamdy khader <murada@mellanox.com>
2017-01-29 04:30:28 -0500

    Added the ability to update client-id dhcp option
    Change-Id: Ie9011b7bd74d1bfcea49effe182fe3e6b6cacddf

--- networking_mlnx/plugins/ml2/drivers/mlnx/mech_mlnx.py
+++ networking_mlnx/plugins/ml2/drivers/mlnx/mech_mlnx.py
@@ -14,10 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from neutron.api.v2 import attributes
 from neutron.plugins.common import constants as p_constants
 from neutron.plugins.ml2 import driver_api as api
 from neutron.plugins.ml2.drivers import mech_agent
+from neutron_lib.api.definitions import extra_dhcp_opt as edo_ext
 from neutron_lib.api.definitions import portbindings
+from neutron_lib.plugins import directory
+from oslo_config import cfg
+
+from networking_mlnx.plugins.ml2.drivers.mlnx import config  # noqa
 
 AGENT_TYPE_MLNX = 'Mellanox plugin agent'
 VIF_TYPE_IB_HOSTDEV = 'ib_hostdev'
@@ -56,3 +62,56 @@ class MlnxMechanismDriver(mech_agent.SimpleAgentMechanismDriverBase):
             context.set_binding(segment[api.ID],
                                 self.vif_type,
                                 self.vif_details)
+
+    def _gen_client_id(self, port):
+        _PREFIX = 'ff:00:00:00:00:00:02:00:00:02:c9:00:'
+        _MIDDLE = ':00:00:'
+        mac_address = port["mac_address"]
+        mac_first = mac_address[:8]
+        mac_last = mac_address[9:]
+        client_id = ''.join([_PREFIX, mac_first, _MIDDLE, mac_last])
+        return client_id
+
+    def _gen_client_id_opt(self, port):
+        client_id = self._gen_client_id(port)
+        return [{"opt_name": edo_ext.DHCP_OPT_CLIENT_ID,
+                 "opt_value": client_id}]
+
+    def _gen_none_client_id_opt(self, port):
+        updated_extra_dhcp_opts = []
+        for opt in port["extra_dhcp_opts"]:
+            opt["opt_value"] = None
+            updated_extra_dhcp_opts.append(opt)
+        return updated_extra_dhcp_opts
+
+    def _process_port_info(self, context):
+        original_port = context.original
+        updated_port = context.current
+        original_host_id = original_port.get("binding:host_id")
+        current_host_id = updated_port.get("binding:host_id")
+        # in case migration did not take place or delete port
+        if original_host_id == current_host_id or current_host_id is None:
+            return
+
+        plugin = directory.get_plugin()
+        # if host_id was empty
+        if not original_port.get("binding:host_id"):
+            print(updated_port)
+            # if port doesn't have extra_dhcp_opts
+            if (not updated_port.get("extra_dhcp_opts") and
+                "compute" in updated_port["device_owner"] and
+                updated_port["binding:vnic_type"] in ("direct", "normal")):
+                updated_port["extra_dhcp_opts"] = \
+                    self._gen_client_id_opt(updated_port)
+        else:
+            if original_port.get("extra_dhcp_opts"):
+                updated_port["extra_dhcp_opts"] = \
+                    self._gen_none_client_id_opt(updated_port)
+        plugin._update_extra_dhcp_opts_on_port(
+            context._plugin_context,
+            updated_port["id"],
+            {attributes.PORT: updated_port})
+
+    def update_port_precommit(self, context):
+        if cfg.CONF.MLNX_IB.update_client_id:
+            self._process_port_info(context)
--- networking_mlnx/tests/unit/ml2/drivers/mlnx/test_mech_mlnx.py
+++ networking_mlnx/tests/unit/ml2/drivers/mlnx/test_mech_mlnx.py
@@ -13,11 +13,23 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import mock
 from neutron.plugins.ml2 import driver_api as api
+# from neutron.tests.tempest.api import base as api_base
 from neutron.tests.unit.plugins.ml2 import _test_mech_agent as base
+from neutron.tests.unit.plugins.ml2 import test_plugin
 from neutron_lib.api.definitions import portbindings
+from neutron_lib import context
+from oslo_config import cfg
+from oslo_config import fixture as fixture_config
+from oslo_utils import uuidutils
 
+from networking_mlnx.plugins.ml2.drivers.mlnx import config
 from networking_mlnx.plugins.ml2.drivers.mlnx import mech_mlnx
+
+
+cfg.CONF.import_group("MLNX_IB",
+                      'networking_mlnx.plugins.ml2.drivers.mlnx')
 
 
 class MlnxMechanismBaseTestCase(base.AgentMechanismBaseTestCase):
@@ -81,12 +93,108 @@ class MlnxMechanismVifDetailsTestCase(MlnxMechanismBaseTestCase):
                           api.PHYSICAL_NETWORK: 'fake_physical_network',
                           api.SEGMENTATION_ID: 1234}]
 
-        context = base.FakePortContext(self.AGENT_TYPE,
-                                       self.AGENTS,
-                                       VLAN_SEGMENTS,
-                                       portbindings.VNIC_DIRECT)
+        _context = base.FakePortContext(self.AGENT_TYPE,
+                                        self.AGENTS,
+                                        VLAN_SEGMENTS,
+                                        portbindings.VNIC_DIRECT)
         segment = VLAN_SEGMENTS[0]
         agent = self.AGENTS[0]
-        self.driver.try_to_bind_segment_for_agent(context, segment, agent)
+        self.driver.try_to_bind_segment_for_agent(_context, segment, agent)
         set({"physical_network": "fake_physical_network"}).issubset(
-            set(context._bound_vif_details.items()))
+            set(_context._bound_vif_details.items()))
+
+
+class FakeContext(base.FakePortContext):
+    def __init__(self, agent_type, agents, segments,
+                 vnic_type=portbindings.VNIC_NORMAL,
+                 original=None, current=None):
+        super(FakeContext, self).__init__(agent_type, agents,
+                                          segments, vnic_type)
+        self._original = original
+        self._current = current
+        self._plugin_context = context.Context('', 'test-tenant')
+
+    @property
+    def current(self):
+        if self._current:
+            return self._current
+        return super(FakeContext, self).current
+
+    @current.setter
+    def set_current(self, value):
+        self._current = value
+
+    @property
+    def original(self):
+        return self._original
+
+    @original.setter
+    def set_original(self, value):
+        self._original = value
+
+
+class MlnxMechanismIbPortTestCase(MlnxMechanismBaseTestCase,
+                                  test_plugin.Ml2PluginV2TestCase):
+                                # api_base.BaseNetworkTest):
+    mechanism_drivers = ['mlnx_infiniband']
+
+    def setUp(self):
+        super(MlnxMechanismIbPortTestCase, self).setUp()
+        self.conf_fixture = self.useFixture(fixture_config.Config())
+        self.conf = self.conf_fixture.conf
+        self.conf.register_opts(config.mlnx_ib_opts, "MLNX_IB")
+        self.conf.set_override('update_client_id', True, "MLNX_IB")
+
+    def _get_context(self):
+        VLAN_SEGMENTS = [{api.ID: 'vlan_segment_id',
+                          api.NETWORK_TYPE: 'vlan',
+                          api.PHYSICAL_NETWORK: 'fake_physical_network',
+                          api.SEGMENTATION_ID: 1234}]
+        original_context = {'binding:host_id': uuidutils.generate_uuid()}
+        current_context = {'id': uuidutils.generate_uuid(),
+                           'binding:host_id': uuidutils.generate_uuid(),
+                           'mac_address': '01:23:45:67:89:ab',
+                           'device_owner': portbindings.VNIC_DIRECT}
+        return FakeContext(self.AGENT_TYPE,
+                           self.AGENTS,
+                           VLAN_SEGMENTS,
+                           portbindings.VNIC_DIRECT,
+                           original=original_context,
+                           current=current_context)
+
+    def test_precommit_ib_port_non_migration(self):
+        _context = self._get_context()
+        _context.original['binding:host_id'] = \
+            _context.current['binding:host_id']
+        self.driver.update_port_precommit(_context)
+        self.assertIsNone(_context.current.get('extra_dhcp_opts'))
+
+    def test_precommit_ib_port_deleted_port(self):
+        _context = self._get_context()
+        _context.current['binding:host_id'] = None
+        self.driver.update_port_precommit(_context)
+        self.assertIsNone(_context.current.get('extra_dhcp_opts'))
+
+    @mock.patch('networking_mlnx.plugins.ml2.drivers.mlnx.mech_mlnx.'
+                'MlnxMechanismDriver._process_port_info')
+    def test_precommit_ib_config_dont_update(self,
+                                             mocked_process_port_info):
+        _context = self._get_context()
+        self.conf.set_override('update_client_id', False, "MLNX_IB")
+        self.driver.update_port_precommit(_context)
+        self.assertFalse(mocked_process_port_info.called)
+
+    def test_percommit_ib_port_update_non_client_id(self):
+        _context = self._get_context()
+        _context.original['extra_dhcp_opts'] = tuple(['some_val'])
+        _context.current['extra_dhcp_opts'] = [{'opt_name': 'some_name',
+                                                'opt_value': 'not_none'}]
+        self.driver.update_port_precommit(_context)
+        self.assertIsNone(_context.current['extra_dhcp_opts'][0]['opt_value'])
+
+    # def test_precommit_ib_port_get_client_id(self):
+    #     _context = self._get_context()
+    #     network = [{api.ID: 'vlan_segment_id'}]
+    #     self.create_port(network, id=_context.current['id'])
+    #     self.driver.update_port_precommit(_context)
+    #     self.assertEqual(_context.current['extra_dhcp_opts'], [])
